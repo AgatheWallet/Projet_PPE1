3114e335c1420933b0f40ed5ef3bf59fe4310f9d
fanfandesbois <fanny.bachey@gmail.com>
2022-11-30 08:58:14 +0100

    script bash

--- scripts/base_tableaux_fichier.sh
+++ scripts/base_tableaux_fichier.sh
@@ -18,7 +18,8 @@ then
 	exit
 fi
 
-mot="robot" # à modifier
+mot="feminismo" # à modifier
+
 
 echo $fichier_urls;
 basename=$(basename -s .txt $fichier_urls)
@@ -27,7 +28,7 @@ echo "<html><body>" > $fichier_tableau
 echo "<h2>Tableau $basename :</h2>" >> $fichier_tableau
 echo "<br/>" >> $fichier_tableau
 echo "<table>" >> $fichier_tableau
-echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th></tr>" >> $fichier_tableau
+echo "<tr><th>ligne</th><th>code</th><th>URL</th><th>encodage</th><th>occurrences</th></tr>" >> $fichier_tableau
 
 lineno=1;
 while read -r URL; do
@@ -35,10 +36,13 @@ while read -r URL; do
 	# la façon attendue, sans l'option -w de cURL
 	code=$(curl -ILs $URL | grep -e "^HTTP/" | grep -Eo "[0-9]{3}" | tail -n 1)
 	charset=$(curl -ILs $URL | grep -Eo "charset=(\w|-)+" | cut -d= -f2)
+	contenu=$(curl $URL) 
 
 	# autre façon, avec l'option -w de cURL
 	# code=$(curl -Ls -o /dev/null -w "%{http_code}" $URL)
 	# charset=$(curl -ILs -o /dev/null -w "%{content_type}" $URL | grep -Eo "charset=(\w|-)+" | cut -d= -f2)
+	
+	nb_occ=$(curl $URL | grep -o 'feminismo' | wc)
 
 	echo -e "\tcode : $code";
 
@@ -53,17 +57,21 @@ while read -r URL; do
 	if [[ $code -eq 200 ]]
 	then
 		dump=$(lynx -dump -nolist -assume_charset=$charset -display_charset=$charset $URL)
+	
+		
 		if [[ $charset -ne "UTF-8" && -n "$dump" ]]
 		then
 			dump=$(echo $dump | iconv -f $charset -t UTF-8//IGNORE)
+	
 		fi
 	else
 		echo -e "\tcode différent de 200 utilisation d'un dump vide"
-		dump=""
-		charset=""
 	fi
+	
+	echo "$dump" >> "dumps-text/$basename-$lineno.txt"
+	echo "$contenu" >> "aspirations/$basename-$lineno.txt"
 
-	echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td></tr>" >> $fichier_tableau
+	echo "<tr><td>$lineno</td><td>$code</td><td><a href=\"$URL\">$URL</a></td><td>$charset</td><td>$nb_occ</td></tr>" >> $fichier_tableau
 	echo -e "\t--------------------------------"
 	lineno=$((lineno+1));
 done < $fichier_urls
